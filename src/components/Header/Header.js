732ad698dc1ab3b822c6af3f5f11d7b02481f180
Miguel Jimenez <mjimenez08@gmail.com>
2020-09-18 16:35:22 -0400

    added header and api

---
+++ src/components/Header/Header.js
@@ -0,0 +1,17 @@
+import React from 'react';
+// import css
+
+function Header() {
+
+    return (
+
+        <div className="header">
+
+            <h1>EMPLOYEE DIRECTORY V1.2</h1>
+
+        </div>
+
+    )
+};
+
+export default Header;
